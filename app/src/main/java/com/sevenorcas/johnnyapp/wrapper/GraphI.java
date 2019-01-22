828382062f69e96e08912701c57a701491959e54
John Stewart <js@7orcas.com>
2019-01-22 18:04:45 +1300

    Running Demo

---
+++ app/src/main/java/com/sevenorcas/johnnyapp/wrapper/GraphI.java
@@ -0,0 +1,11 @@
+package com.sevenorcas.johnnyapp.wrapper;
+
+public interface GraphI {
+    final static public String STATE_MAX         = "mx";
+    final static public String STATE_LAST        = "lx";
+    final static public String STATE_DATA_POINTS = "dp";
+
+    final static public String STATE_DELIMIT_1   = ",";
+    final static public String STATE_DELIMIT_2   = "=";
+    final static public String STATE_DELIMIT_3   = "|";
+}
